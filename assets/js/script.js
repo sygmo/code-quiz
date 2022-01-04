0d230eeb78dd58cdee71e66ff76f1d192e9effbe
Sydney Mercier <sydney.mercier91@gmail.com>
2022-01-04 17:30:14 -0600

    created intro view and nav bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
-console.log("Hiya!");
+// create array of question objects
+
+// user clicks start button
+
+// start timer
+
+// present next question
+
+// when question answered incorrectly, subtract time from the clock
+
+// game over when timer reaches 0
+
+// save initials and score
