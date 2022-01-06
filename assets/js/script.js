5c96e723bff6cdef372b65fcf995798fc1240b72
Sydney Mercier <sydney.mercier91@gmail.com>
2022-01-06 17:19:22 -0600

    created array of question objects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,46 @@
 // create array of question objects
+const questions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        option1: "strings",
+        option2: "booleans",
+        option3: "alerts",
+        option4: "numbers",
+        correct: "option3"
+    },
+    {
+        question: "The condition in an if / else statement is enclosed within _____.",
+        option1: "quotes",
+        option2: "curly brackets",
+        option3: "parentheses",
+        option4: "square brackets",
+        correct: "option3"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store _____.",
+        option1: "numbers and strings",
+        option2: "other arrays",
+        option3: "booleans",
+        option4: "all of the above",
+        correct: "option4"
+    },
+    {
+        question: "String values must be enclosed within _____ when being assigned to variables.",
+        option1: "commas",
+        option2: "curly brackets",
+        option3: "quotes",
+        option4: "parentheses",
+        correct: "option3"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        option1: "JavaScript",
+        option2: "terminal / bash",
+        option3: "for loops",
+        option4: "console.log",
+        correct: "option4"
+    }
+]
 
 // user clicks start button
 
